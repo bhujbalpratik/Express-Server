c2d8b54a607cba0070215d554f2ae411cf342510
bhujbal pratik <pratikbhujbal743@gmail.com>
2024-03-14 14:48:07 +0530

    Add new pre-production changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,44 +1,44 @@
-import { User } from "../server.js";
+import { User } from "../index.js"
 
 export const userHome = (req, res) => {
-  res.render("userhome", { title: "User Home Page" });
-};
+  res.render("userhome", { title: "User Home Page" })
+}
 
 export const userGetRegister = (req, res) => {
-  res.render("userregister", { title: "Register Page" });
-};
+  res.render("userregister", { title: "Register Page" })
+}
 
 export const userPostRegister = async (req, res) => {
-  const { name, email, password } = req.body;
+  const { name, email, password } = req.body
   await User.create({
     name,
     email,
     password,
-  });
-  return res.redirect("/user/login");
-};
+  })
+  return res.redirect("/user/login")
+}
 
 export const userGetLogin = (req, res) => {
-  res.render("userlogin", { title: "Login Page" });
-};
+  res.render("userlogin", { title: "Login Page" })
+}
 
 export const userPostLogin = async (req, res) => {
-  const { password, email } = req.body;
+  const { password, email } = req.body
 
-  const user = await User.findOne({ email });
+  const user = await User.findOne({ email })
 
-  if (!user) return res.redirect("/user/register");
+  if (!user) return res.redirect("/user/register")
 
-  const isPasswordMatch = user.password === password;
+  const isPasswordMatch = user.password === password
 
   if (!isPasswordMatch)
     return res.render("userlogin", {
       title: "Login Page",
       msg: "Wrong Password. Try again",
-    });
+    })
 
   return res.render("index", {
     title: "Home Page",
     msg: `Hello, ${user.name}`,
-  });
-};
+  })
+}
